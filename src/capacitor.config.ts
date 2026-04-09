2321bb2351bc86a75f46e90aadf29c85a36515f2
vickyswami206-glitch <vickyswami206@gmail.com>
2026-04-09 18:03:12 +0530

    Change appId to lowercase in capacitor.config.ts

--- src/capacitor.config.ts
+++ src/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.Ai.Friday',
+  appId: 'com.ai.friday',
   appName: 'Friday',
   webDir: 'dist'
 };
