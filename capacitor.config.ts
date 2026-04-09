ba6e36e205e2bb75f47971791170085909d77503
vickyswami206-glitch <vickyswami206@gmail.com>
2026-04-09 20:39:15 +0530

    Add initial Capacitor configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ai.friday',
+  appName: 'Friday',
+  webDir: 'dist'
+};
+
+export default config;
