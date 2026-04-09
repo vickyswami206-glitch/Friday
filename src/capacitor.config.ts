fb913ab9fcdbbed65085cfef5164e7c6e8f1cdd2
vickyswami206-glitch <vickyswami206@gmail.com>
2026-04-09 20:40:14 +0530

    Delete src/capacitor.config.ts

--- src/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.ai.friday',
-  appName: 'Friday',
-  webDir: 'dist'
-};
-
-export default config;
