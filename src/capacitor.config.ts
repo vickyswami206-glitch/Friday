7c072c77c622a3a5e1776782ed9b8b0a1b4bb33d
vickyswami206-glitch <vickyswami206@gmail.com>
2026-04-09 17:12:01 +0530

    Create capacitor.config.ts

---
+++ src/capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ai.appointment',
+  appName: 'AI Appointment',
+  webDir: 'dist'
+};
+
+export default config;
